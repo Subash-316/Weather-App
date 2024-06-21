ec15c686aeb29d978e44675da7e019dc20d25161
subash <subashdx7@gmail.com>
2024-06-21 14:17:47 +0530

    project

---
+++ myproject/weatherapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from .form import CityForm
+from .models import City
+from django.contrib import messages
+import requests
+
+# Create your views here.
+def home(request):
+    url='https://api.openweathermap.org/data/2.5/weather?q={},&appid=14144903d04c3001e19168b4923e6c10&units=metric'
+    
+    if request.method=="POST":
+        form=CityForm(request.POST) 
+        if form.is_valid():
+            NCity=form.cleaned_data['name']
+            CCity=City.objects.filter(name=NCity).count()
+            if CCity==0:
+                res=requests.get(url.format(NCity)).json()
+                if res['cod']==200:
+                    form.save()
+                    messages.success(request," "+NCity+" Added Successfully .....")
+                else:
+                    messages.error(request," city does not exist ......")
+            else:
+                messages.error(request," city already exist ......") 
+    form=CityForm()
+    cities=City.objects.all()
+    data=[]
+    for city in cities:
+        res=requests.get(url.format(city)).json()
+
+        city_weather={
+            'city':city,
+            'temperature' : res['main']['temp'],
+            'description' : res['weather'][0]['description'],
+            'country' : res['sys']['country'],
+            'icon' : res['weather'][0]['icon'],
+        }
+        data.append(city_weather)
+    context={'data' : data,'form':form}
+    return render(request,"weatherapp.html",context)
+
+def delete_city(request,CName):
+    City.objects.get(name=CName).delete()
+    messages.success(request," "+CName+" removed successfully ...")
+    return redirect("Home")
